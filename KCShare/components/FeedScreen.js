ae27a6c8d7b0adf66b2a51f065b8046a68de4141
Marco Angeli <marco.angeli@mail.utoronto.ca>
2020-03-01 18:51:59 -0500

    Changed look of app, connected it to AWS

--- KCShare/components/FeedScreen.js
+++ KCShare/components/FeedScreen.js
@@ -1,12 +1,52 @@
-import { Text, View } from 'react-native';
 import React, { Component } from 'react';
+import { SafeAreaView, View, FlatList, StyleSheet, Text } from 'react-native';
+import Constants from 'expo-constants';
+import Amplify from 'aws-amplify';
+
+let DATA = [] 
+
+Amplify.API.get('getPosts', "").then( (response) => {
+    DATA.push.apply(DATA, response);
+    console.log(DATA)
+
+}).catch((error) => {
+    console.log(error)
+})
+
+function Post({ content }) {
+  return (
+    <View style={styles.post}>
+      <Text style={styles.content}>{content}</Text>
+    </View>
+  );
+}
 
 export default class FeedScreen extends Component {
   render() {
       return (
-	  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-	      <Text>Feed!</Text>
-	  </View>
-      )
+	  <SafeAreaView style={styles.container}>
+	      <FlatList
+		  data={DATA}
+		  renderItem={({ item }) => <Post content={item.content} />}
+		  keyExtractor={post => post.id}
+	      />
+	  </SafeAreaView>
+      );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  post: {
+    backgroundColor: '#23275f',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  content: {
+    fontSize: 32,
+    color: '#fcfcff',
+  },
+});
