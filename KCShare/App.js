1fc391ff1ef4dfd52234f55ee901e026f066a8ba
Marco Angeli <marco.angeli@mail.utoronto.ca>
2020-03-01 14:01:10 -0500

    Added header

--- KCShare/App.js
+++ KCShare/App.js
@@ -3,25 +3,26 @@ import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { MaterialCommunityIcons } from 'react-native-vector-icons';
+import { Header } from 'react-native-elements'
 import FeedScreen from './components/FeedScreen.js'
 import NewPostScreen from './components/NewPostScreen.js'
 import ProfileScreen from './components/ProfileScreen.js'
 
 const Tab = createMaterialBottomTabNavigator();
 
-function MyTabs() {
+function MyTabs(props) {
     let iconSize = 23;
-  return (
-      <Tab.Navigator
-	  initialRouteName="Feed"
-	  labelStyle={{ fontSize: 12 }}
-	  barStyle={{ backgroundColor: 'red' }}
-	  style={{ backgroundColor: 'blue' }}
-      >
-	<Tab.Screen
-          name="Feed"
-          component={FeedScreen}
-          options={{
+    return (
+	<Tab.Navigator
+	    initialRouteName="Feed"
+	    labelStyle={{ fontSize: 12 }}
+	    barStyle={{ backgroundColor: 'red' }}
+	    style={{ backgroundColor: 'blue' }}
+	>
+	    <Tab.Screen
+		name="Feed"
+		component={FeedScreen}
+		options={{
           tabBarLabel: 'Home',
           tabBarIcon: ({focused, color, size }) => (
             <MaterialCommunityIcons name="home" color={color} size={iconSize} />
@@ -55,6 +56,15 @@ function MyTabs() {
 export default function App() {
   return (
     <NavigationContainer>
+	<Header
+	    leftComponent={{ icon: 'menu', color: '#fff' }}
+	    centerComponent={{ text: 'KCShare', style: { color: '#fff' } }}
+	    rightComponent={{ icon: 'home', color: '#fff' }}
+	    containerStyle={{
+		backgroundColor: 'red',
+		justifyContent: 'space-around',
+	    }}
+	/>
       <MyTabs />
     </NavigationContainer>
   );
