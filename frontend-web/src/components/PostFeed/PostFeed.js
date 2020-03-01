47f1a2873b8da00b9e361618cb49a3104f250db7
Joshua Bragg <joshua.bragg@outlook.com>
2020-03-01 15:53:07 -0500

    Added backend post fetching

--- frontend-web/src/components/PostFeed/PostFeed.js
+++ frontend-web/src/components/PostFeed/PostFeed.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { uid } from "react-uid";
 import './PostFeed.css';
+import Amplify from 'aws-amplify';
 
 import Post from '../Post/Post'
 
@@ -9,51 +10,46 @@ class PostFeed extends React.Component {
 		posts: []
 	}
 
-	getPosts = () => {
-		const posts = [{
-				id: 1,
-				location: 'Toronto, ON',
-				content: 'Aenean imperdiet. Quisque rutrum. In hac habitasse platea dictumst. Quisque ut nisi. Etiam imperdiet imperdiet orci. Maecenas ullamcorper, dui et placerat feugiat, eros pede varius nisi, condimentum viverra felis nunc et lorem. Phasellus volutpat, metus eget egestas mollis, lacus lacus blandit dui, id egestas quam mauris ut lacus. Curabitur a felis in nunc fringilla tristique. Maecenas malesuada. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum.',
-				uploadTime: 1582653096
-			},
-			{
-				id: 2,
-				location: 'Ottawa, ON',
-				content: 'Sed magna purus, fermentum eu, tincidunt eu, varius ut, felis. Praesent venenatis metus at tortor pulvinar varius. Fusce fermentum odio nec arcu. Vivamus elementum semper nisi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
-				uploadTime: 1582653096
-			},
-			{
-				id: 3,
-				location: 'Montreal, QC',
-				content: 'Maecenas malesuada. Sed mollis, eros et ultrices tempus, mauris ipsum aliquam libero, non adipiscing dolor urna a orci. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Vestibulum facilisis, purus nec pulvinar iaculis, ligula mi congue nunc, vitae euismod ligula urna in dolor. Phasellus viverra nulla ut metus varius laoreet.',
-				uploadTime: 1582653096
-			},
-			{
-				id: 4,
-				location: 'Toronto, ON',
-				content: 'Aenean imperdiet. Quisque rutrum. In hac habitasse platea dictumst. Quisque ut nisi. Etiam imperdiet imperdiet orci. Maecenas ullamcorper, dui et placerat feugiat, eros pede varius nisi, condimentum viverra felis nunc et lorem. Phasellus volutpat, metus eget egestas mollis, lacus lacus blandit dui, id egestas quam mauris ut lacus. Curabitur a felis in nunc fringilla tristique. Maecenas malesuada. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum.',
-				uploadTime: 1582653096
-			},
-			{
-				id: 5,
-				location: 'Ottawa, ON',
-				content: 'Sed magna purus, fermentum eu, tincidunt eu, varius ut, felis. Praesent venenatis metus at tortor pulvinar varius. Fusce fermentum odio nec arcu. Vivamus elementum semper nisi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
-				uploadTime: 1582653096
-			},
-			{
-				id: 6,
-				location: 'Montreal, QC',
-				content: 'Maecenas malesuada. Sed mollis, eros et ultrices tempus, mauris ipsum aliquam libero, non adipiscing dolor urna a orci. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Vestibulum facilisis, purus nec pulvinar iaculis, ligula mi congue nunc, vitae euismod ligula urna in dolor. Phasellus viverra nulla ut metus varius laoreet.',
-				uploadTime: 1582653096
-			}];
+	constructor() {
+		super();
 
-		this.setState({ posts: posts });
+		Amplify.configure({
+			API: {
+				endpoints: [{
+					name: 'getPosts',
+					endpoint: 'https://720phsp0e7.execute-api.us-east-1.amazonaws.com/dev/getPosts',
+					service: 'api-gateway',
+					region: 'us-east-1'
+				}]
+			}
+		});
+	}
+
+	getPosts = () => {
+		Amplify.API.get('getPosts', '', { headers: {} }).then((response) => {
+			const posts = [];
+
+			console.log(response);
+
+			response.forEach((post) => {
+				posts.push({
+					id: post.postID,
+					location: post.location,
+					content: post.content,
+					uploadTime: post.timeUploaded
+				});
+			});
+
+			this.setState({ posts: posts });
+		}).catch((error) => {
+			console.log(error);
+		});
 	}
 
 	componentWillMount() {
 		const feedType = this.props.feedType;
 
-		this.getPosts();
+		this.getPosts(feedType);
 	}
 
 	render() {
