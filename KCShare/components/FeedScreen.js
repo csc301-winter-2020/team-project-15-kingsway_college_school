64ee2c32343cf52563b26ebaadedba7322851ac5
Marco Angeli <marco.angeli@mail.utoronto.ca>
2020-03-03 01:45:43 -0500

    Added Feed header, properly formatted some of the code

--- KCShare/components/FeedScreen.js
+++ KCShare/components/FeedScreen.js
@@ -2,41 +2,66 @@ import React, { Component } from 'react';
 import { SafeAreaView, View, FlatList, StyleSheet, Text } from 'react-native';
 import Constants from 'expo-constants';
 import Amplify from 'aws-amplify';
-import Post from './Post.js'
+import Post from './Post.js';
 
- export default class FeedScreen extends Component {
-     state = {
-	 posts: []
-     }
+class FeedHeader extends Component {
+    render() {
+	return (
+	    <View style={styles.header}>
+		<Text style={styles.headerText}>KCShare</Text>
+	    </View>
+	)
+    }
+}
 
-     componentDidMount() {
-	 if (this.state.posts.length === 0) {
-	     Amplify.API.get('getPosts', "").then( (response) => {
-		 this.setState({posts: response});
-		 console.log(this.state.posts[0])
-	     }).catch((error) => {
-		 console.log(error)
-	     })
-	 }
-     }
+export default class FeedScreen extends Component {
+    state = {
+	posts: []
+    }
 
-     render() {
-	 
-		 return (
-	     <SafeAreaView style={styles.container}>
-	       <FlatList
-		 data={this.state.posts}
-		 renderItem={({ item }) => <Post content={item.content} />}
-		 keyExtractor={post => post.id}
-		 />
-	     </SafeAreaView>
-	 );
-     }
- }
+    componentDidMount() {
+	if (this.state.posts.length === 0) {
+	    Amplify.API.get('getPosts', "").then( (response) => {
+		this.setState({posts: response});
+		console.log(this.state.posts[0])
+	    }).catch((error) => {
+		console.log(error)
+	    })
+	}
+    }
+
+    render() {
+	
+	return (
+	    <View style={styles.view}>
+		<FeedHeader/>
+		<SafeAreaView style={styles.container}>
+		    <FlatList
+			data={this.state.posts}
+			renderItem={({ item }) => <Post content={item.content} />}
+			keyExtractor={post => post.id}
+		    />
+		</SafeAreaView>
+	    </View>
+	);
+    }
+}
 
 const styles = StyleSheet.create({
-    container: {
+    view: {
 	flex: 1,
 	backgroundColor: '#110d41'
     },
+    container: {
+	flex: 8,
+    },
+    header: {
+	flex:1,
+    },
+    headerText: {
+	fontSize: 30,
+	fontWeight: 'bold',
+	color: '#fcfcff',
+	padding: 25,
+    }
 });
