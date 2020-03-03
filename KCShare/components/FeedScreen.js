4daa5fb8419424b0626c9f5885a5a424ad54264a
Marco Angeli <marco.angeli@mail.utoronto.ca>
2020-03-03 00:39:26 -0500

    Changed Feed look, comparmentalized Post so other screen can use it

--- KCShare/components/FeedScreen.js
+++ KCShare/components/FeedScreen.js
@@ -2,51 +2,41 @@ import React, { Component } from 'react';
 import { SafeAreaView, View, FlatList, StyleSheet, Text } from 'react-native';
 import Constants from 'expo-constants';
 import Amplify from 'aws-amplify';
+import Post from './Post.js'
 
-let DATA = [] 
+ export default class FeedScreen extends Component {
+     state = {
+	 posts: []
+     }
 
-Amplify.API.get('getPosts', "").then( (response) => {
-    DATA.push.apply(DATA, response);
-    console.log(DATA)
+     componentDidMount() {
+	 if (this.state.posts.length === 0) {
+	     Amplify.API.get('getPosts', "").then( (response) => {
+		 this.setState({posts: response});
+		 console.log(this.state.posts[0])
+	     }).catch((error) => {
+		 console.log(error)
+	     })
+	 }
+     }
 
-}).catch((error) => {
-    console.log(error)
-})
-
-function Post({ content }) {
-  return (
-    <View style={styles.post}>
-      <Text style={styles.content}>{content}</Text>
-    </View>
-  );
-}
-
-export default class FeedScreen extends Component {
-  render() {
-      return (
-	  <SafeAreaView style={styles.container}>
-	      <FlatList
-		  data={DATA}
-		  renderItem={({ item }) => <Post content={item.content} />}
-		  keyExtractor={post => post.id}
-	      />
-	  </SafeAreaView>
-      );
-  }
-}
+     render() {
+	 
+		 return (
+	     <SafeAreaView style={styles.container}>
+	       <FlatList
+		 data={this.state.posts}
+		 renderItem={({ item }) => <Post content={item.content} />}
+		 keyExtractor={post => post.id}
+		 />
+	     </SafeAreaView>
+	 );
+     }
+ }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  post: {
-    backgroundColor: '#23275f',
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-  },
-  content: {
-    fontSize: 32,
-    color: '#fcfcff',
-  },
+    container: {
+	flex: 1,
+	backgroundColor: '#110d41'
+    },
 });
---
+++ KCShare/components/Post.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Text, View, StyleSheet } from "react-native"
+
+export default class Post extends Component {
+    render() {
+	return (
+	<View style={styles.post}>
+	    <Text style={styles.content}>{this.props.content}</Text>
+	</View>
+	)
+    }
+}
+
+const styles = StyleSheet.create({
+   post: {
+     backgroundColor: '#23275f',
+     padding: 20,
+     marginVertical: 8,
+     marginHorizontal: 16,
+     borderRadius: 10
+   },
+   content: {
+     fontSize: 15,
+     color: '#fcfcff',
+   },
+ });
+
