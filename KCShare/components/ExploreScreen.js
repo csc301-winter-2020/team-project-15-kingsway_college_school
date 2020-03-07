bfa12274562197e351fe820a25b4f8ed3583ed5e
Marco Angeli <marco.angeli@mail.utoronto.ca>
2020-03-07 17:37:10 -0500

    Reworked bottom bar

---
+++ KCShare/components/ExploreScreen.js
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native';
+import React, { Component } from 'react';
+
+export default class ExploreScreen extends Component {
+  render() {
+      return (
+	  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+	      <Text>Explore</Text>
+	  </View>
+      )
+  }
+}
