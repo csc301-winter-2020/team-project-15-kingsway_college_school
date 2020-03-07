8d822dddcc229a63b6ba421aff18ab97bf1f9d2b
Joshua Bragg <joshua.bragg@outlook.com>
2020-03-07 18:43:34 -0500

    Create post is ready to call backend

--- frontend-web/src/components/CreatePost/CreatePost.js
+++ frontend-web/src/components/CreatePost/CreatePost.js
@@ -3,19 +3,39 @@ import { uid } from "react-uid";
 import './CreatePost.css';
 
 class CreatePost extends React.Component {
-	state = {}
+	state = {
+		postData: '',
+		attachment: null
+	}
+
+	postDataChanged = (e) => {
+		this.setState({ postData: e.target.value });
+	}
+
+	fileUploaded = (e) => {
+		this.setState({ attachment: e.target.files[0] });
+	}
+
+	handleSubmit = (e) => {
+		e.preventDefault();
+
+		console.log(this.state.postData, this.state.attachment)
+	}
 
 	render() {
 		return (
+		<form onSubmit={ this.handleSubmit }>
 		<div className="CreatePost light-grey-text">
 			<div className="TextAreaContainer shadow">
-				<textarea id="new-post-textarea" onChange={ this.TextAreaChange } placeholder="Share an experience"/>
+				<textarea id="new-post-textarea" onChange={ this.postDataChanged } placeholder="Share an experience"/>
 			</div>
 			<div className="CreatePostButtons">
-				<div className="AttachPicture fa fa-paperclip"></div>
-				<button className="ShareButton shadow light-grey dark-grey-text">Share</button>
+				<input id="fileUpload" type="file" name="file" className="hidden" onChange={ this.fileUploaded }/>
+				<label for="fileUpload" className="AttachPicture fa fa-paperclip"></label>
+				<input type="submit" className="ShareButton shadow light-grey dark-grey-text" value="Share" />
 			</div>
 		</div>
+		</form>
 	)}
 };
 
