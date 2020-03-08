8cf6a4833b0b3b1f2f6e8f4977c7790a3c159d65
Nick Perrin <nick.perrin@hotmail.com>
2020-03-08 13:02:32 -0400

    Beginnings of explore screen (mobile)

--- frontend-mobile/components/ExploreScreen.js
+++ frontend-mobile/components/ExploreScreen.js
@@ -1,12 +1,71 @@
-import { Text, View } from 'react-native';
 import React, { Component } from 'react';
+import { SafeAreaView, View, FlatList, StyleSheet, Text, TextInput, TouchableOpacity } from 'react-native';
+import Constants from 'expo-constants';
+import Amplify from 'aws-amplify';
+import Post from './Post.js';
+import { SearchBar } from 'react-native-elements';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default class ExploreScreen extends Component {
+class ExploreHeader extends Component {
   render() {
-      return (
-	  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-	      <Text>Explore</Text>
-	  </View>
-      )
+    return (
+      <View style={styles.header}>
+        <View>
+            <Text style={styles.headerText}>Top Tags</Text>
+        </View>
+        <View style={{flex: 1}}>
+          <SearchBar autoFocus 
+            containerStyle={styles.searchBarContainer}
+            inputStyle={styles.searchBarInput}
+            inputContainerStyle={styles.searchBarInputContainer}
+          />
+        </View>
+      </View>
+    )
   }
 }
+
+export default class ExploreScreen extends Component {
+  componentDidMount() {
+
+  }
+
+
+  render() {
+    return (
+      <View style={styles.view}>
+        <ExploreHeader navigation={this.props.navigation} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+    backgroundColor: "#110d41"
+  },
+  container: {
+    flex: 8
+  },
+  header: {
+    flex: 1,
+    flexDirection: "row"
+  },
+  headerText: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#fcfcff",
+    padding: 25
+  },
+  searchBarContainer: {
+    flex: 1,
+    backgroundColor: "#110d41",
+    paddingTop: 25,
+    paddingBottom: 10
+  },
+  searchBarInputContainer: {
+    backgroundColor: "#110d41"
+  },
+  searchBarInput: {}
+});
