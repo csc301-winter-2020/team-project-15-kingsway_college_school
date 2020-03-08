2d4e8049f45ed8942af7dc6e3a629a2e54e60a4c
Ryan Marten <ryanmarten@Ryans-MacBook-Pro-2.local>
2020-03-08 13:26:37 -0400

    Zoom is correctly configured for Map in Explore

--- frontend-web/src/components/Explore/Explore.js
+++ frontend-web/src/components/Explore/Explore.js
@@ -15,18 +15,21 @@ class Explore extends React.Component {
 	// 	});
 	// }
 
+
 	componentDidMount(){
 			var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
 			mapboxgl.accessToken = 'pk.eyJ1Ijoicnlhbm1hcnRlbiIsImEiOiJjazc5aDZ6Zmgwcno0M29zN28zZHQzOXdkIn0.aXAWfSB_yY8MzA2DajzgBQ';
 			var map = new mapboxgl.Map({
 			container: 'map', // container id
 			style: 'mapbox://styles/mapbox/streets-v11',
-			center: [-74.5, 40], // starting position
-			zoom: 9 // starting zoom
+			center: [-79.3949, 43.6529], // starting position
+			zoom: 10.99 // starting zoom
 			});
 			
 			// Add zoom and rotation controls to the map.
 			map.addControl(new mapboxgl.NavigationControl());
+
+			
 	}
 
 	render() {
@@ -34,7 +37,7 @@ class Explore extends React.Component {
 		return (
 		<div className="Explore dark-grey light-grey-text">
 			Explore Page NEW!
-			<div id="map" height="100px" width="100px"></div>
+			<div id="map"></div>
 		</div>
 	)}
 };
