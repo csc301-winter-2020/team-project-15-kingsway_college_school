a39a5a670097db67bebfae9160601fddf41cc288
Marco Angeli <marco.angeli@mail.utoronto.ca>
2020-03-15 15:21:14 -0400

    Configure AWS for signin

--- frontend-mobile/App.js
+++ frontend-mobile/App.js
@@ -11,36 +11,79 @@ import NewPostScreen from './components/NewPostScreen.js'
 import ProfileScreen from './components/ProfileScreen.js'
 import LoginScreen from './components/LoginScreen.js'
 import Amplify from 'aws-amplify';
-
+import { withAuthenticator } from 'aws-amplify-react-native'; // or 'aws-amplify-react-native';
 
 Amplify.configure({
     API: {
 	endpoints: [
 	    {
 		name: 'getPosts',
-		endpoint: 'https://720phsp0e7.execute-api.us-east-1.amazonaws.com/prod/getPosts',
+		endpoint: 'https://720phsp0e7.execute-api.us-east-1.amazonaws.com/dev/getPosts',
 		service: 'api-gateway',
 		region: 'us-east-1'
 	    },
 	    {
 		name: 'getPopularHashtags',
-		endpoint: 'https://720phsp0e7.execute-api.us-east-1.amazonaws.com/prod/getPopularHashtags',
+		endpoint: 'https://720phsp0e7.execute-api.us-east-1.amazonaws.com/dev/getPopularHashtags',
 		service: 'api-gateway',
 		region: 'us-east-1',
 	    },
 	    
 	    {
 		name: 'newPost',
-		endpoint: 'https://720phsp0e7.execute-api.us-east-1.amazonaws.com/prod/newPost',
+		endpoint: 'https://720phsp0e7.execute-api.us-east-1.amazonaws.com/dev/newPost',
 		service: 'api-gateway',
 		region: 'us-east-1'
 	    },
 	    {
 		name: 'deletePost',
-		endpoint: 'https://720phsp0e7.execute-api.us-east-1.amazonaws.com/prod/deletePost',
+		endpoint: 'https://720phsp0e7.execute-api.us-east-1.amazonaws.com/dev/deletePost',
 		service: 'api-gateway',
 		region: 'us-east-1'
 	}]
+    },
+    Auth: {
+
+        // REQUIRED - Amazon Cognito Region
+        region: 'us-east-1',
+
+        // OPTIONAL - Amazon Cognito User Pool ID
+        userPoolId: 'us-east-1_mgyiyKSzT',
+
+        // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+        userPoolWebClientId: '1lnuahrib0rj0c2gs84o9nsu1i',
+
+        // OPTIONAL - Enforce user authentication prior to accessing AWS resources or not
+        mandatorySignIn: true,
+
+        // OPTIONAL - Configuration for cookie storage
+        // Note: if the secure flag is set to true, then the cookie transmission requires a secure protocol
+        cookieStorage: {
+        // REQUIRED - Cookie domain (only required if cookieStorage is provided)
+            domain: 'kcsshare.auth.us-east-1.amazoncognito.com',
+        // OPTIONAL - Cookie path
+            path: '/',
+        // OPTIONAL - Cookie expiration in days
+            expires: 365,
+        // OPTIONAL - Cookie secure flag
+        // Either true or false, indicating if the cookie transmission requires a secure protocol (https).
+            secure: true
+        },
+
+        // OPTIONAL - Manually set the authentication flow type. Default is 'USER_SRP_AUTH'
+        authenticationFlowType: 'USER_PASSWORD_AUTH',
+
+        // OPTIONAL - Manually set key value pairs that can be passed to Cognito Lambda Triggers
+        clientMetadata: { myCustomKey: 'myCustomValue' },
+
+         // OPTIONAL - Hosted UI configuration
+        oauth: {
+            domain: 'your_cognito_domain',
+            scope: ['phone', 'email', 'profile', 'openid', 'aws.cognito.signin.user.admin'],
+            redirectSignIn: 'http://localhost:3000/',
+            redirectSignOut: 'http://localhost:3000/',
+            responseType: 'code' // or 'token', note that REFRESH token will only be generated when the responseType is code
+        }
     }
 });
 
@@ -102,7 +145,7 @@ function MyTabs(props) {
 }
 const Stack = createStackNavigator();
 
-export default function App() {
+function App() {
     return (
 	<View style={styles.view}>
 	    <NavigationContainer style={styles.bar}>
@@ -127,6 +170,9 @@ export default function App() {
 	</View>
     );
 }
+
+export default withAuthenticator(App);
+
 const styles = StyleSheet.create({
     view: {
 	flex: 1,
