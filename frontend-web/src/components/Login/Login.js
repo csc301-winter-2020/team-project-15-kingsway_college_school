eb8ff07a71a1b9a195f4eb482f55e01b82bee7e9
Joshua Bragg <joshua.bragg@outlook.com>
2020-03-21 18:54:11 -0400

    Completed UI for login page

--- frontend-web/src/components/Login/Login.js
+++ frontend-web/src/components/Login/Login.js
@@ -5,6 +5,7 @@ class Login extends React.Component {
 	state = {}
 
 	handleSubmit = (e) => {
+		e.preventDefault()
 		console.log(e)
 	}
 
@@ -14,14 +15,24 @@ class Login extends React.Component {
 			<div className="Login dark-grey light-grey-text">
 				<div className="FormBox mid-grey shadow">
 					<div className="LoginTitle">Login</div>
-					<div className="LoginField">
-						<input type="text" placeholder=""/>
+					<div className="LoginEmail">
+						<div className="LoginSubtitle">
+							Email
+						</div>
+						<div className="LoginField shadow">
+							<input type="text" placeholder=""/>
+						</div>
 					</div>
-					<div className="LoginField">
-						<input type="password" placeholder=""/>
+					<div className="LoginPassword">
+						<div className="LoginSubtitle">
+							Password
+						</div>
+						<div className="LoginField shadow">
+							<input type="password" placeholder=""/>
+						</div>
 					</div>
+					<input type="submit" className="LoginSubmit mid-mid-grey light-grey-text shadow"/>
 				</div>
-				<input type="submit" className="hidden"/>
 			</div>
 		</form>
 	)}
