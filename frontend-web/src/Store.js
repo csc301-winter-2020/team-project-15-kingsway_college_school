b96685cc36362b796aabc5b94088f168eb616d21
DCastaban <devincastaban@gmail.com>
2020-03-21 16:54:21 -0400

    Implemented retreiving images from the client through the aws sdk

--- frontend-web/src/Store.js
+++ frontend-web/src/Store.js
@@ -1,10 +1,12 @@
 import { decorate, observable, action, configure } from "mobx"
 import React from "react"
+import { Auth } from 'aws-amplify';
 
 class Store extends React.Component {
 
 	currentView = 'Home'
-
+	user = null;
+	session = null; 
 	search = (searchTerm) => {
 		console.error('[SEARCH NOT DEFINED]')
 	}
@@ -27,20 +29,58 @@ class Store extends React.Component {
 	updateFeeds = () => {
 		this.updateFeedCallback.forEach((f) => { f() });
 	}
+	SignIn = async (username, password) => {
+		try {
+			const user = await Auth.signIn(username, password);
+			if (user.challengeName === 'NEW_PASSWORD_REQUIRED') {
+				const {requiredAttributes} = user.challengeParam; // the array of required attributes, e.g ['email', 'phone_number']
+				const loggedUser = await Auth.completeNewPassword(
+					user,              // the Cognito User Object
+					password,       // the new password
+					// OPTIONAL, the required attributes
+				);
+			} else {
+				// The user directly signs in
+				this.user = user
+				this.session = user.signInUserSession
+			}
+		} catch (err) { 
+			console.log(err);
+			if (err.code === 'UserNotConfirmedException') {
+				// The error happens if the user didn't finish the confirmation step when signing up
+				// In this case you need to resend the code and confirm the user
+				// About how to resend the code and confirm the user, please check the signUp part
+			} else if (err.code === 'PasswordResetRequiredException') {
+				// The error happens when the password is reset in the Cognito console
+				// In this case you need to call forgotPassword to reset the password
+				// Please check the Forgot Password part.
+			} else if (err.code === 'NotAuthorizedException') {
+				// The error happens when the incorrect password is provided
+			} else if (err.code === 'UserNotFoundException') {
+				// The error happens when the supplied username/email does not exist in the Cognito user pool
+			} else {
+				console.log(err);
+			}
+		}
+	}
+
 
 }
 
 decorate(Store, {
 	currentView: observable,
 	updateFeedCallback: observable,
+	user: observable,
+	session: observable,
 	updateFeeds: action,
 	setCurrentView: action,
 	changeTab: action,
 	refreshCurrentView: action,
-	search: action
+	search: action,
+	signIn: action
 })
 
 // You can remove this if you want to be able to directly change values rather than doing it through a setter function
 configure({enforceActions: "observed"})
-const globalStore = new Store()
+const globalStore = observable(new Store())
 export default globalStore;
--- frontend-web/src/components/PostFeed/PostFeed.js
+++ frontend-web/src/components/PostFeed/PostFeed.js
@@ -2,77 +2,31 @@ import React from 'react';
 import { uid } from "react-uid";
 import './PostFeed.css';
 import Amplify from 'aws-amplify';
-import { Auth } from 'aws-amplify';
+import { Auth, Storage } from 'aws-amplify';
 import Post from '../Post/Post'
 import Loader from '../Loader/Loader'
-let username = "devin"
-let password = "bing0Bang@@"
-let email = "asdf@gmail.com"
-class PostFeed extends React.Component {
+import {  observer } from "mobx-react"
+
+
+const PostFeed = observer(class PostFeed extends React.Component {
 	state = {
 		hasPosts: false,
 		posts: []
 	}
-
-	getPosts = (feedType, searchTerm) => {
+	callPostsApi = async (session, feedType, searchTerm) => {
 		Amplify.configure({
 			API: {
 				endpoints: [{
 					name: 'getPosts',
-					endpoint: 'https://720phsp0e7.execute-api.us-east-1.amazonaws.com/prod/getPosts',
+					endpoint: 'https://720phsp0e7.execute-api.us-east-1.amazonaws.com/dev/getPosts',
 					service: 'api-gateway',
 					region: 'us-east-1'
 				}]
-			},
-			Auth: {
-		
-				// REQUIRED - Amazon Cognito Region
-				region: 'us-east-1',
-		
-				// OPTIONAL - Amazon Cognito User Pool ID
-				userPoolId: 'us-east-1_jXw5z0sO3',
-		
-				// OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
-				userPoolWebClientId: '2be70uebsba896oah66e7gduua',
-		
-				// OPTIONAL - Enforce user authentication prior to accessing AWS resources or not
-				mandatorySignIn: true,
-		
-				// OPTIONAL - Configuration for cookie storage
-				// Note: if the secure flag is set to true, then the cookie transmission requires a secure protocol
-				// cookieStorage: {
-				// // REQUIRED - Cookie domain (only required if cookieStorage is provided)
-				//     domain: 'kcsshare.auth.us-east-1.amazoncognito.com',
-				// // OPTIONAL - Cookie path
-				//     path: '/',
-				// // OPTIONAL - Cookie expiration in days
-				//     expires: 365,
-				// // OPTIONAL - Cookie secure flag
-				// // Either true or false, indicating if the cookie transmission requires a secure protocol (https).
-				//     secure: true
-				// },
-		
-				// OPTIONAL - Manually set the authentication flow type. Default is 'USER_SRP_AUTH'
-				authenticationFlowType: 'USER_PASSWORD_AUTH',
-		
-				// OPTIONAL - Manually set key value pairs that can be passed to Cognito Lambda Triggers
-				// clientMetadata: { myCustomKey: 'myCustomValue' },
-		
-				 // OPTIONAL - Hosted UI configuration
-				// oauth: {
-				//     domain: 'your_cognito_domain',
-				//     scope: ['phone', 'email', 'profile', 'openid', 'aws.cognito.signin.user.admin'],
-				//     redirectSignIn: 'http://localhost:3000/',
-				//     redirectSignOut: 'http://localhost:3000/',
-				//     responseType: 'code' // or 'token', note that REFRESH token will only be generated when the responseType is code
-				// }
 			}
-		});
-		this.SignIn()
+		})
 		let getParams = {};
 
 		const userID = '2';
-
 		if (feedType === 'Home') {
 			if (searchTerm) {
 				getParams = { queryStringParameters: { searchType: 'TAG', searchParameter: searchTerm } };
@@ -82,88 +36,77 @@ class PostFeed extends React.Component {
 		} else if (feedType === 'Favourites') {
 			getParams = { queryStringParameters: { searchType: 'FAV', searchParameter: userID } };
 		}
-
-		Amplify.API.get('getPosts', '', getParams).then((response) => {
+		getParams["headers"] = {"Authorization" : session.idToken.jwtToken}
+		let currCreds
+		Auth.currentCredentials().then(response => {
+					currCreds = response
+		})
+		await Amplify.API.get('getPosts', '', getParams).then((response) => {
 			const posts = [];
 
 			if (Object.entries(response).length === 0 && response.constructor === Object) {
 				response = [];
 			}
 
-			response.forEach((post) => {
-				posts.push({
-					postID: post.postID,
-					userID: post.userID,
-					location: post.location,
-					content: post.content,
-					images: post.images,
-					uploadTime: post.timeUploaded,
+			if(response.length != 0){
+				response.forEach((post, outerIndex) => {
+					post.images.map( async (imageKey, innerIndex) => {
+						let imageKeyUrl;
+						var imageBase64; 
+						let imageUrl;
+						let headers = { "headers"  : { "Authorization" : session.idToken.jwtToken}}
+						// await Storage.get(imageKey, { download: true }).then(result =>  console.log(result))
+						var aws = require("aws-sdk")
+						currCreds = await Auth.currentCredentials();
+						aws.config.update({region: 'us-east-1', credentials: currCreds});
+						const s3 = new aws.S3(); // Pass in opts to S3 if necessary
+						var getParams = {
+							Bucket: 'kcpostimages', // your bucket name,
+							Key: imageKey, // path to the object you're looking for
+						}
+	
+	
+						await s3.getObject(getParams, (err, data) => {
+							// Handle any error and exit
+							if (err){
+								console.log(err)
+								return err;
+							}
+							// No error happened
+							// Convert Body from a Buffer to a String
+							let objectData = data.Body.toString('utf-8'); // Use the encoding necessary
+							imageBase64 = objectData
+							this.setState({ hasPosts: false });
+
+							this.state.posts[outerIndex].images[innerIndex] = imageBase64
+							this.setState({ hasPosts: true });
+							this.forceUpdate()
+						});
+					})
+					posts.push({
+						postID: post.postID,
+						userID: post.userID,
+						location: post.location,
+						content: post.content,
+						images: post.images,
+						uploadTime: post.timeUploaded,
+					});
 				});
-			});
+			}
 
 			this.setState({ posts: posts, hasPosts: true });
+			this.forceUpdate()
 		}).catch((error) => {
 			console.log(error);
 		});
 	}
+	getPosts = async (feedType, searchTerm) => {
+		// Janky solution for waiting until authenticated		
+		setTimeout( () => {
+			this.callPostsApi(this.props.store.session, feedType, searchTerm)
+		}, 2000)
 
-	SignIn = async () => {
-		try {
-			const user = await Auth.signIn(username, password);
-			if (user.challengeName === 'SMS_MFA' ||
-				user.challengeName === 'SOFTWARE_TOKEN_MFA') {
-				// You need to get the code from the UI inputs
-				// and then trigger the following function with a button click
-				// const code = getCodeFromUserInput();
-				// If MFA is enabled, sign-in should be confirmed with the confirmation code
-				// const loggedUser = await Auth.confirmSignIn( 
-				// 	user,   // Return object from Auth.signIn()
-				// 	code,   // Confirmation code  
-				// 	mfaType // MFA Type e.g. SMS_MFA, SOFTWARE_TOKEN_MFA
-				// );
-			} else if (user.challengeName === 'NEW_PASSWORD_REQUIRED') {
-				const {requiredAttributes} = user.challengeParam; // the array of required attributes, e.g ['email', 'phone_number']
-				// You need to get the new password and required attributes from the UI inputs
-				// and then trigger the following function with a button click
-				// For example, the email and phone_number are required attributes
-				// const {username, email, phone_number} = getInfoFromUserInput();
-				const loggedUser = await Auth.completeNewPassword(
-					user,              // the Cognito User Object
-					password,       // the new password
-					// OPTIONAL, the required attributes
-					// {
-					// 	email : email,
-					// }
-					//     phone_number,
-					// } 
-				);
-			} else if (user.challengeName === 'MFA_SETUP') {
-				// This happens when the MFA method is TOTP
-				// The user needs to setup the TOTP before using it
-				// More info please check the Enabling MFA part
-				Auth.setupTOTP(user);
-			} else {
-				// The user directly signs in
-				// console.log(user);
-			}
-		} catch (err) { 
-			console.log(err);
-			if (err.code === 'UserNotConfirmedException') {
-				// The error happens if the user didn't finish the confirmation step when signing up
-				// In this case you need to resend the code and confirm the user
-				// About how to resend the code and confirm the user, please check the signUp part
-			} else if (err.code === 'PasswordResetRequiredException') {
-				// The error happens when the password is reset in the Cognito console
-				// In this case you need to call forgotPassword to reset the password
-				// Please check the Forgot Password part.
-			} else if (err.code === 'NotAuthorizedException') {
-				// The error happens when the incorrect password is provided
-			} else if (err.code === 'UserNotFoundException') {
-				// The error happens when the supplied username/email does not exist in the Cognito user pool
-			} else {
-				console.log(err);
-			}
-		}
+			
 	}
 	search = (searchTerm) => {
 		this.setState({ hasPosts: false, posts: [] });
@@ -196,6 +139,6 @@ class PostFeed extends React.Component {
 			}
 		</div>
 	)}
-};
+})
 
 export default PostFeed;
