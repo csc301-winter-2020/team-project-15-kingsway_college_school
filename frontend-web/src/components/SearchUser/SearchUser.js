06175070b45290d06530020c07e3cab63f903fdd
Joshua Bragg <joshua.bragg@outlook.com>
2020-03-22 22:37:28 -0400

    Created searchuser component | caught error being called when searching for posts while authenticating

---
+++ frontend-web/src/components/SearchUser/SearchUser.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './SearchUser.css';
+
+class SearchUser extends React.Component {
+	state = {}
+
+	render() {
+		return (
+		<div className="SearchUser">
+			
+		</div>
+	)}
+};
+
+export default SearchUser;
