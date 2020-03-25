67655dea6646747412da9f4fb3348603bcf76cef
Marco Angeli <marco.angeli@mail.utoronto.ca>
2020-03-24 22:23:39 -0400

    Added image selection and UI support

--- frontend-mobile/components/NewPostScreen.js
+++ frontend-mobile/components/NewPostScreen.js
@@ -1,164 +1,198 @@
 import React, { Component, useState } from 'react';
-import { TouchableWithoutFeedback, Keyboard, Alert, CameraRoll, Image, View, FlatList, StyleSheet, Text, TextInput, KeyboardAvoidingView} from 'react-native';
+import { TouchableWithoutFeedback, Keyboard, Alert, Image, View, StyleSheet, Text, TextInput, KeyboardAvoidingView } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Icon } from 'react-native-elements';
-import Constants from 'expo-constants';
 import Amplify from 'aws-amplify';
-import Post from './Post.js';
-import { TabActions } from '@react-navigation/native';
-//import Geolocation from 'react-native-geolocation-service';
+import * as ImagePicker from 'expo-image-picker';
+import Constants from 'expo-constants';
 
 const side_margins = 16
 const Tab = createBottomTabNavigator();
 
 // New Post Header for the page
 class NewPostHeader extends Component {
-    render() {
-	return (
-	    <View style={styles.header}>
-		<Text style={styles.headerText}>New Post</Text>
-	    </View>
-	)
-    }
+	render() {
+		return (
+			<View style={styles.header}>
+				<Text style={styles.headerText}>New Post</Text>
+			</View>
+		)
+	}
 }
 
 class NewPostBody extends Component {
-    constructor() {
-	super();
-	this.newInputText = this.newInputText.bind(this)
-	this.getLocation = this.getLocation.bind(this)
-	this.getPhotosFromGallery = this.getPhotosFromGallery.bind(this)
+	constructor() {
+		super();
+		this.newInputText = this.newInputText.bind(this)
+		this.getLocation = this.getLocation.bind(this)
+		this.getPhotosFromGallery = this.getPhotosFromGallery.bind(this)
+		this.getPermissionAsync = this.getPermissionAsync.bind(this)
 
-    }
-    state = {
-	text: ''
 	}
-	
-    getLocation() {
-	if (hasLocationPermission) {
-	    Geolocation.getCurrentPosition(
-		// Add capability to link location here.
-		(position) => {
-		    console.log(position);
-		},
-		(error) => {
-		    // See error code charts below.
-		    console.log(error.code, error.message);
-		},
-		{ enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
-	    );
+	state = {
+		text: '',
+		//image: 'file:///data/user/0/host.exp.exponent/cache/ExperienceData/%2540anonymous%252FKCShare-ec3dbb19-618e-4e08-bdc7-32f8400d4d4a/ImagePicker/d5f0dbf7-fbe7-4acb-a9cc-d0edfafa2e74.jpg'
+		image: null
 	}
-    }
-
-    getPhotosFromGallery() {
-	CameraRoll.getPhotos({ first: 1000000 })
-	// TODO: Implement the getPhotos call for iPhone and Android
-		  .then(res => {
-		      let photoArray = res.edges;
-		      this.setState({ showPhotoGallery: true, photoArray: photoArray })
-		  })
-    }
-
-    newInputText(text) {
-	this.state.text = text
-    }
-
-    submitPost() {
-	console.log(this.state.text)
-	console.log("Submitting Post");
-	const reqParams = { body: { content: this.state.text } };
 
 
-	Amplify.API.post('newPost', '', reqParams).then((response) => {
-	    console.log("Post submitted")
-	    Alert.alert("Post submitted", ":)")
-	}).catch((error) => {
-	    console.log(error);
-	    console.log(error.response)
-	});
-	Keyboard.dismiss();
-	this.textInput.clear();
+	getLocation() {
+		if (hasLocationPermission) {
+			Geolocation.getCurrentPosition(
+				// Add capability to link location here.
+				(position) => {
+					console.log(position);
+				},
+				(error) => {
+					// See error code charts below.
+					console.log(error.code, error.message);
+				},
+				{ enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+			);
+		}
+	}
+	getPermissionAsync = async () => {
+		if (Constants.platform.ios) {
+			const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+			if (status !== 'granted') {
+				alert('Sorry, we need camera roll permissions to make this work!');
+			}
+		}
+	}
 
-    }
+	async getPhotosFromGallery() {
+		let result = await ImagePicker.launchImageLibraryAsync({
+			mediaTypes: ImagePicker.MediaTypeOptions.All,
+			allowsEditing: true,
+			aspect: [4, 3],
+			quality: 1
+		});
 
-    render() {
-	return (
-	    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-	    <KeyboardAvoidingView style={{flex:7}} resetScrollToCoords={{ x: 0, y: 0}}>
-		
-			<TextInput
-				ref={input => { this.textInput = input }}
-			style={styles.TextInputStyleClass}
-			placeholder={"Share what you've learnt!"}
-			placeholderTextColor="#9E9E9E"
-			borderBottomColor='#000000'
-			numberOfLines={10}
-			multiline={true}
-			onChangeText={(text) => this.newInputText(text)}
-			/>
+		console.log(result);
 
-			<View style={{flexDirection: 'row', justifyContent: "space-between"}}>
-				<View style={styles.addFiles}>
-				<Icon name="location-on" color="#2A5AD5" raised={true} reverse={true} onPress={() => this.getLocation()}/>
-				<Icon name="camera-alt" color="#2A5AD5" raised={true} reverse={true} onPress={() => this.getPhotosFromGallery()}/>
-				</View>
+		if (!result.cancelled) {
+			this.setState({ image: result.uri });
+		}
+	}
 
-				<View style={styles.submitPost}>
-				<Icon name="check" color="#28B11D" raised={true} reverse={true} onPress={() => this.submitPost()}/>
-				</View>
+	newInputText(text) {
+		this.state.text = text
+	}
 
-			</View>
-	    </KeyboardAvoidingView>
-	    </TouchableWithoutFeedback>
-	)
-    } 
+	submitPost() {
+		console.log(this.state.text)
+		console.log("Submitting Post");
+		const reqParams = { body: { content: this.state.text } };
+
+
+		Amplify.API.post('newPost', '', reqParams).then((response) => {
+			console.log("Post submitted")
+			Alert.alert("Post submitted", ":)")
+		}).catch((error) => {
+			console.log(error);
+			console.log(error.response)
+		});
+		Keyboard.dismiss();
+		this.textInput.clear();
+
+	}
+
+	componentDidMount() {
+		this.getPermissionAsync();
+		console.log('hi');
+	}
+
+	render() {
+		console.log("State:")
+		console.log(this.state)
+		let image = null;
+		if (this.state.image != null) {
+			image = <Image style={styles.image} source={{ uri: this.state.image }} />
+		}
+		return (
+			<TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+				<KeyboardAvoidingView style={{ flex: 7 }} resetScrollToCoords={{ x: 0, y: 0 }}>
+					<View style={{ flex: this.state.image == null ? 1 : 2, backgroundColor: "#292654", borderRadius: 20, marginHorizontal: side_margins, padding: 20, }}>
+						<View style={{ flex: 1 }}>
+							<TextInput
+								ref={input => { this.textInput = input }}
+								style={styles.TextInputStyleClass}
+								placeholder={"Share what you've learnt!"}
+								placeholderTextColor="#9E9E9E"
+								borderBottomColor='#000000'
+								numberOfLines={10}
+								multiline={true}
+								onChangeText={(text) => this.newInputText(text)}
+							/>
+
+						</View>
+						{image}
+					</View>
+
+					<View style={{ flexDirection: 'row', justifyContent: "space-between", flex: 1 }}>
+
+						<View style={styles.addFiles}>
+							<Icon name="location-on" color="#2A5AD5" raised={true} reverse={true} onPress={() => this.getLocation()} />
+							<Icon name="camera-alt" color="#2A5AD5" raised={true} reverse={true} onPress={() => this.getPhotosFromGallery()} />
+						</View>
+
+						<View style={styles.submitPost}>
+							<Icon name="check" color="#28B11D" raised={true} reverse={true} onPress={() => this.submitPost()} />
+						</View>
+
+					</View>
+				</KeyboardAvoidingView>
+			</TouchableWithoutFeedback >
+		)
+	}
 }
 
 export default class NewPostScreen extends Component {
-    render() {
-	return (
-	    <View style={styles.view}>
-		<NewPostHeader/>
-		<NewPostBody/>
-	    </View>
+	render() {
+		return (
+			<View style={styles.view}>
+				<NewPostHeader />
+				<NewPostBody />
+			</View>
 
-	)
-    }
+		)
+	}
 }
 
 const styles = StyleSheet.create({
-    view: {
-	flex: 1,
-	backgroundColor: '#110d41'
-    },
-    header: {
-	flex:1,
-	marginTop: side_margins/2,
-    },
-    headerText: {
-	fontSize: 30,
-	fontWeight: 'bold',
-	color: '#fcfcff',
-	padding: 25,
-    },
-    TextInputStyleClass: { 
-	textAlign: 'left',
-	textAlignVertical: 'top',
-	color:'#FFFFFF',
-	height: 200,
-	borderRadius: 20,
-	backgroundColor : "#292654",
-	padding: 20,
-	marginHorizontal: side_margins,
-    },
-    addFiles: {
-	flexDirection: 'row',
-	justifyContent: 'flex-start',
-	marginHorizontal: side_margins,
-    },
-    submitPost: {
-	flexDirection:'row',
-	justifyContent: 'flex-end',
-	marginRight: 10,
-    },
+	view: {
+		flex: 1,
+		backgroundColor: '#110d41'
+	},
+	header: {
+		flex: 1,
+		marginTop: side_margins / 2,
+	},
+	headerText: {
+		fontSize: 30,
+		fontWeight: 'bold',
+		color: '#fcfcff',
+		padding: 25,
+	},
+	TextInputStyleClass: {
+		textAlign: 'left',
+		textAlignVertical: 'top',
+		color: '#FFFFFF',
+	},
+	addFiles: {
+		flexDirection: 'row',
+		justifyContent: 'flex-start',
+		marginHorizontal: side_margins,
+	},
+	submitPost: {
+		flexDirection: 'row',
+		justifyContent: 'flex-end',
+		marginRight: 10,
+	},
+	image: {
+		flex: 1,
+		height: null,
+		width: null,
+		resizeMode: "contain",
+	}
 });
